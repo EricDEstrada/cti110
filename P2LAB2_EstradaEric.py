5ff98f191de7c26f8a07a2d0e66475f5a0d77bf0
EricDEstrada <99384644+EricDEstrada@users.noreply.github.com>
2022-02-21 21:06:23 -0600

    Update P2LAB2_EstradaEric.py
    Mortage assignment

--- P2LAB2_EstradaEric.py
+++ P2LAB2_EstradaEric.py
@@ -4,6 +4,7 @@ last_months_price = int(input())
 ''' Type your code here. '''
 change = current_price - last_months_price #Should calculate the change in price
 
-print('This house is $'+str(current_price)+'.'' The change is $'+str(change)+' since last month.')
+print('This house is ${}.'' The change is ${} since last month.'.format(current_price,change)) 
 print('The estimated monthly mortgage is ${:.2f}.'.format(current_price*0.051/12))
+
 
